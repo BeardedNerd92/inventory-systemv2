cf354246cacc84e61df82e533b7cd1014ff5d4e4
BeardedNerd92 <IntMain92@gmail.com>
2026-02-04 12:18:46 -0600

    chore: added inventory.py and manage.py

--- inventory_system_v2/inventory_system_v2/settings.py
+++ inventory_system_v2/inventory_system_v2/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_p81-7xb6l&&%l))ldtd*rnf_+t7g^$pzt9r6^ycp)a%6!!9fv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
